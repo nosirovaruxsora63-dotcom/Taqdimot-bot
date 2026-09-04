d585a75fc4fedbdb04aa2282223f836bd00c4acd
nosirovaruxsora63-dotcom <nosirovaruxsora63@gmail.com>
2026-09-04 13:44:47 +0500

    Update main.py

--- main.py
+++ main.py
@@ -16,8 +16,8 @@ from pptx.util import Inches, Pt
 
 load_dotenv()
 
-TOKEN = os.getenv("AQ.Ab8RN6Iu6riqs8tEMVX_qo83q8CcYRaNLrCNliWfF9tz4fD3mg")
-GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+TOKEN = os.getenv("Sizning_BotFather_dan_olgan_tokeningiz")
+GROQ_API_KEY = os.getenv("Sizning_Groq_api_kalitingiz")
 
 bot = Bot(
     token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML)
